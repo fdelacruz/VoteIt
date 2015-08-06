219b26312800a07ba2d8744bd4ea5b7298f1ede1
Francisco De La Cruz <helloquico@gmail.com>
2015-08-06 19:05:25 -0400

    Build Process With Gulp and Browserify

--- src/app.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var React = require('react'),
-	MessageBox = require('./MessageBox');
-
-var reactComponent = React.renderComponent(
-  <MessageBox />,
-  document.getElementById('app')
-);
